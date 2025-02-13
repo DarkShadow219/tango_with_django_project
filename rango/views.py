551fa41cca932b17a391808ff2964ffca7e98172
Nehatul Islam <2998107i@student.gla.ac.uk>
2025-02-13 13:21:47 +0000

    Final Commit All Completed

--- rango/views.py
+++ rango/views.py
@@ -150,7 +150,7 @@ def some_view(request):
     
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
